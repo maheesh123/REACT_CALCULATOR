6d78d9e215b6c818a9c4f0f596eacbd6c280275d
maheesh123 <139621164+maheesh123@users.noreply.github.com>
2024-09-29 20:33:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+ 
+import './App.css';
+import Main from './Main';
+import './Style.css'
+
+function App() {
+  return(
+  <Main/>
+  )
+}
+
+
+export default App;
---
+++ Main.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Main = () => {
+    function display (value){
+        setInputValue(inputValue+value)
+    }
+    
+    function calculator(){
+        // let answers = eval(inputValue)
+        setInputValue(eval(inputValue))
+    }
+    function clear(){
+        setInputValue("")
+    }
+
+    const [inputValue,setInputValue] = useState("")
+  return (
+    <> 
+      <form name="calc" className="calculator">
+        <input type="text" className='value' value={inputValue} />
+        <span className="num clear" onClick={()=>clear()}>C</span>
+        <span onClick={()=>display("*")}>*</span>
+        <span onClick={()=>display("7")}>7</span>
+        <span onClick={()=>display("8")}>8</span>
+        <span onClick={()=>display("/")}>/</span>
+        <span onClick={()=>display("9")}>9</span>
+        <span onClick={()=>display("-")}>-</span>
+        <span onClick={()=>display("4")}>4</span>
+        <span onClick={()=>display("5")}>5</span>
+        <span onClick={()=>display("6")}>6</span>
+        <span className='plus' onClick={()=>display("+")}>+</span>
+        <span onClick={()=>display("1")}>1 </span>
+        <span onClick={()=>display("2")}>2 </span>
+        <span onClick={()=>display("3")}>3 </span>
+        <span onClick={()=>display("0")}>0 </span>
+        <span onClick={()=>display("00")}>00</span>
+        <span onClick={()=>display(".")}>.</span>
+        <span onClick={()=>calculator() }className='num equal'>=</span>
+      </form>
+
+    </>
+  )
+}
+
+export default Main
